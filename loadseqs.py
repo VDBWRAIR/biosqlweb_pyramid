4276f2caad5b3ff208323da33965391727879c5f
averagehat <michael.panciera.work@gmail.com>
2016-01-14 16:35:53 -0500

    misspelling fix

--- loadseqs.py
+++ loadseqs.py
@@ -16,7 +16,7 @@ db = server[dbname]
 parser = GenBank.FeatureParser()
 loadgb = lambda _id: db.load(GenBank.Iterator(Entrez.efetch(db='nucleotide', id=_id, rettype='gb', retmode='text'), parser))
 
-ACCESSIONS_FILE = 'accessions.lst' if len(sys.argv) < 2 else sys.argv[1]
+ACCESSIONS_FILE = 'accession.lst' if len(sys.argv) < 2 else sys.argv[1]
 for id in open(ACCESSIONS_FILE):
     print "Loading %s" % id
     loadgb(id)
